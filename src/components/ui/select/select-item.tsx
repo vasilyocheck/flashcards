06df0303aabfc858840993cf17d1a46b12bc3af9
camperfail86 <mega.kochkin@inbox.ru>
2024-01-10 21:11:37 +0500

    select item styles

--- src/components/ui/select/select-item.tsx
+++ src/components/ui/select/select-item.tsx
@@ -1,21 +1,18 @@
 import React, { ComponentPropsWithoutRef, ElementType } from 'react'
 
-import { CheckIcon } from '@radix-ui/react-icons'
 import * as Select from '@radix-ui/react-select'
 
 import s from './select.module.scss'
 
-type Props<T extends ElementType = 'input'> = {
+type Props<T extends ElementType = 'div'> = {
   className?: string
+  value: string
 } & ComponentPropsWithoutRef<T>
 
-export const SelectItem = React.forwardRef(({ children, className, ...props }: Props) => {
+export const SelectItem = React.forwardRef(({ children, className, value, ...props }: Props) => {
   return (
-    <Select.Item className={s.SelectItem} {...props}>
+    <Select.Item className={s.SelectItem} value={value} {...props}>
       <Select.ItemText>{children}</Select.ItemText>
-      <Select.ItemIndicator className={'SelectItemIndicator'}>
-        <CheckIcon />
-      </Select.ItemIndicator>
     </Select.Item>
   )
 })
