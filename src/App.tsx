f69870a7f1efbe435d9d7861f2c0cd561a43c991
camperfail86 <mega.kochkin@inbox.ru>
2024-01-10 22:51:17 +0500

    select logic function

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import { SelectComponent } from '@/components/ui/select'
 const arrayElements = ['Vitaliy', 'Kirill', 'Vasiliy']
 const arrayNumbers = ['100', '50', '10']
 
+import React from 'react'
+
 import s from './components/ui/select/select.module.scss'
 
 function App() {
+  const [value, setValue] = React.useState(arrayNumbers[0])
+
   return (
     <div>
-      <span>Hello, my name is Kirill</span>
-      <div>hello</div>
+      <span>Hello, my name is {value}</span>
       <Checkbox />
       <div
         style={{
@@ -18,8 +21,13 @@ function App() {
           justifyContent: 'center',
         }}
       >
-        <SelectComponent array={arrayElements} />
-        <SelectComponent array={arrayNumbers} className={s.SelectNumbers} />
+        <SelectComponent array={arrayElements} callback={setValue} value={value} />
+        <SelectComponent
+          array={arrayNumbers}
+          callback={setValue}
+          className={s.SelectNumbers}
+          value={value}
+        />
       </div>
     </div>
   )
