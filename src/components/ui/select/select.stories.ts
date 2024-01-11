edaaa9e04148e30ea22f0a35d5ffdb8ce06fd0f6
camperfail86 <mega.kochkin@inbox.ru>
2024-01-11 18:49:57 +0500

    select stories refactoring

--- src/components/ui/select/select.stories.ts
+++ src/components/ui/select/select.stories.ts
@@ -1,6 +1,8 @@
 import { SelectComponent } from '@/components/ui/select/select'
 import { Meta, StoryObj } from '@storybook/react'
 
+import s from './select.module.scss'
+
 const meta: Meta<typeof SelectComponent> = {
   component: SelectComponent,
   title: 'Components/Select',
@@ -12,5 +14,12 @@ type Story = StoryObj<typeof meta>
 export const SelectComponentMain: Story = {
   args: {
     array: ['Vasiliy', 'Kirill', 'Vitaliy'],
+  },
+}
+
+export const SelectComponentNumbers: Story = {
+  args: {
+    array: ['10', '50', '100'],
+    className: s.SelectNumbers,
   },
 }
