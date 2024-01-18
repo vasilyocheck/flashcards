92d70f1a064a5fc83adeb876be31ae83dae4c73b
vasilyocheck <vasily.medvedev1986@gmail.com>
2024-01-18 07:25:46 +0300

    create sign up form

---
+++ src/components/auth/sign-up-form/sign-up-form.stories.ts
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { SignUpForm } from './sign-up-form'
+
+const meta = {
+  component: SignUpForm,
+  tags: ['autodocs'],
+  title: 'Auth/SignUpForm',
+} satisfies Meta<typeof SignUpForm>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const SignUpFormSample: Story = {}
