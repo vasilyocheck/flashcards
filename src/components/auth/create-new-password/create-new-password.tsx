9aedc80f2c3c77a9e0e202a8a425b9b57cdb3a27
camperfail86 <mega.kochkin@inbox.ru>
2024-01-18 22:18:37 +0500

    create-new-password and refactor components

--- src/components/auth/create-new-password/create-new-password.tsx
+++ src/components/auth/create-new-password/create-new-password.tsx
@@ -1,16 +1,25 @@
-// type Props = {}
-
 import { useForm } from 'react-hook-form'
 
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { TextField } from '@/components/ui/textfield'
 import { Typography } from '@/components/ui/typography'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
 
 import s from './create-new-password.module.scss'
 
 export const CreateNewPassword = () => {
-  const { handleSubmit, register } = useForm()
+  const passwordSchema = z.object({
+    password: z.string().min(3, { message: 'Must be 3 or more characters long' }),
+  })
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm({
+    resolver: zodResolver(passwordSchema),
+  })
   const onSubmit = (data: any) => {
     console.log(data)
   }
@@ -18,10 +27,17 @@ export const CreateNewPassword = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <Card>
-        <span style={{ marginBottom: '33px', textAlign: 'center' }}>
-          <Typography variant={'large'}>Create new password</Typography>
-        </span>
-        <TextField {...register('Password')} label={'Password'} type={'password'}></TextField>
+        <Typography className={s.title} variant={'large'}>
+          Create new password
+        </Typography>
+        <div className={s.textField}>
+          <TextField {...register('password')} label={'Password'} type={'password'}></TextField>
+          {errors.password?.message && (
+            <Typography className={s.errorMessage} variant={'error'}>
+              {errors.password.message}
+            </Typography>
+          )}
+        </div>
         <Typography className={s.text} variant={'body2'}>
           Create new password and we will send you further instructions to email
         </Typography>
