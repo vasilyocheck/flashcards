2a2b74aa5a2cfcddad4b8a3c752b0f3ff1eccac6
vasilyocheck <vasily.medvedev1986@gmail.com>
2024-01-28 22:26:49 +0300

    create via use state

--- src/components/decks/add-new-deck.tsx
+++ src/components/decks/add-new-deck.tsx
@@ -1,63 +1,59 @@
-import { useForm } from 'react-hook-form'
+import { ChangeEvent, EventHandler, useState } from 'react'
 
 import { ImageIcon } from '@/assets'
 import { Button } from '@/components/ui/button'
 import { Checkbox } from '@/components/ui/checkbox'
-import { ControlledTextField } from '@/components/ui/controlled/controlled-textfield'
 import { Modal } from '@/components/ui/modal'
+import { TextField } from '@/components/ui/textfield'
 import { useAddDeckMutation } from '@/services/base-api'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { z } from 'zod'
 
 import sModal from '@/components/ui/modal/modal.module.scss'
 
-const addDeckSchema = z.object({
-  isPrivate: z.boolean(),
-  name: z.string(),
-})
-
-type FormValues = z.infer<typeof addDeckSchema>
-
 export const AddNewDeck = () => {
-  const {
-    control,
-    formState: { errors },
-    handleSubmit,
-    register,
-  } = useForm<FormValues>({ resolver: zodResolver(addDeckSchema) })
   const [addDeck] = useAddDeckMutation()
-  const onSubmit = (data: FormValues) => {
-    console.log(data)
+  const [name, setName] = useState('')
+  const [textfieldError, setTextFieldError] = useState('')
+
+  const handleOnTextfieldChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (textfieldError && name.length > 4) {
+      setTextFieldError('')
+    }
+    setName(e.currentTarget.value)
+  }
+
+  const handleAddNewPack = () => {
+    if (name.length < 5) {
+      setTextFieldError('Your deck name shall be at least 5 characters')
+    } else {
+      console.log({ name })
+    }
   }
 
   return (
     <Modal nameButton={'Add New Deck'} title={'Add New Deck'} width={'542px'}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <ControlledTextField
-          control={control}
-          errorMessage={errors.name?.message}
-          label={'Name Pack'}
-          name={'name'}
-        />
-        <Button className={'uploadButton'} fullWidth variant={'secondary'}>
-          <ImageIcon size={1} />
-          Upload Image
+      <TextField
+        errorMessage={textfieldError}
+        label={'Name Pack'}
+        onChange={handleOnTextfieldChange}
+        value={name}
+      />
+      <Button className={'uploadButton'} fullWidth variant={'secondary'}>
+        <ImageIcon size={1} />
+        Upload Image
+      </Button>
+      <div className={sModal.checkbox}>
+        <Checkbox label={'Private pack'} />
+      </div>
+      <div className={sModal.buttons}>
+        <Button
+          onClick={() => console.log('close window func')}
+          type={'reset'}
+          variant={'secondary'}
+        >
+          Cancel
         </Button>
-        <div className={sModal.checkbox}>
-          <Checkbox {...register('isPrivate')} />
-          {/*<Checkbox label={'Private pack'} />*/}
-        </div>
-        <div className={sModal.buttons}>
-          <Button
-            onClick={() => console.log('close window func')}
-            type={'reset'}
-            variant={'secondary'}
-          >
-            Cancel
-          </Button>
-          <Button type={'submit'}>Add New Pack</Button>
-        </div>
-      </form>
+        <Button onClick={handleAddNewPack}>Add New Pack</Button>
+      </div>
     </Modal>
   )
 }
